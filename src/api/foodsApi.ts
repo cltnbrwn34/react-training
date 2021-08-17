7071e630b8d029b96df5fb2ef9a404915318e2f2
Colton Brown <cltnbrwn34@gmail.com>
2021-08-17 15:22:50 -0400

    adding delete food API

--- src/api/foodsApi.ts
+++ src/api/foodsApi.ts
@@ -2,13 +2,20 @@ export async function getFood() {
     return fetch ("http://localhost:3001/inventory");
 }
 
+export async function deleteFood(id:number) {
+    const response =  await fetch ("http://localhost:3001/inventory/" + id, {
+        method: "DELETE",
+    });
+    if (!response.ok){
+        throw new Error("Delete failed!")
+    }
+    return await (await response).json();
+}
+
 export async function createFood() {
     return fetch ("http://localhost:3001/inventory");
 }
 
 export async function updateFood() {
-    return fetch ("http://localhost:3001/inventory");
-}
-export async function deleteFood() {
     return fetch ("http://localhost:3001/inventory");
 }
