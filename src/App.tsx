d35f3bfec314a9b7366ad83971d91ec805d0b8b0
Colton Brown <cltnbrwn34@gmail.com>
2021-08-17 13:07:04 -0400

    adding aray via function call

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
-export function App() {
-    return <h1>Restaurant Manager</h1>
+
+type Food = {
+    name: string;
+    quantity: number;
+};
+
+const inventory: Food[] = [
+    { name: "Carrot", quantity: 3 },
+    { name: "Potato", quantity: 8 },
+];
+
+export function App(props: any) {
+    function renderInventory() {
+        return inventory.map(food => <li>{food.name}</li>)
+    }
+    return (
+    <>
+        <h1>Restaurant Manager</h1>
+        <ul>
+            { renderInventory()}
+        </ul>
+    </>);
 }
