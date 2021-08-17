8970b0b2ae9126d8d801ac8fc77b4d1c7b9dfaca
Colton Brown <cltnbrwn34@gmail.com>
2021-08-17 15:01:02 -0400

    mock get foods call

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,53 @@
-
+import { getFood } from "./api/foodsApi"
+import { useEffect, useState} from "react"
 type Food = {
+    id: number
     name: string;
     quantity: number;
+    minimumQuantity: number;
+    type: string;
 };
 
-const inventory: Food[] = [
-    { name: "Carrot", quantity: 3 },
-    { name: "Potato", quantity: 8 },
-];
-
 export function App(props: any) {
-    function renderInventory() {
-        return inventory.map(food => <li>{food.name}</li>)
-    }
+
+    const [inventory, setFoods] = useState<Food[]>([]);
+
+    useEffect(() => {
+        async function callGetFood(){
+        const response = await getFood();
+        if (!response.ok) throw new Error("Call to getFood failed!");
+        const json = await response.json();
+        setFoods(json);
+        }
+        callGetFood();
+    });
+    
     return (
     <>
-        <h1>Restaurant Manager</h1>
-        <ul>
-            { renderInventory()}
-        </ul>
-    </>);
+      <h1>Restaurant Manager</h1>
+      <ul>
+        {/* exercise 1: display quantity next to food with a dash in between */}
+        <table>
+          <thead>
+              <tr>
+                <th>Qty.</th>
+                <th>Name</th>
+                <th>Min Qty.</th>
+                <th>Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            {inventory.map((food) => (
+                <tr key={food.id}>
+                    <td>{food.quantity}</td>
+                    <td>{food.name}</td>
+                    <td>{food.minimumQuantity}</td>
+                    <td>{food.type}</td>
+                </tr>
+            ))}
+          </tbody>
+        </table>
+      </ul>
+    </>
+  );
 }
---
+++ src/api/foodsApi.ts
@@ -0,0 +1,14 @@
+export async function getFood() {
+    return fetch ("http://localhost:3001/inventory");
+}
+
+export async function createFood() {
+    return fetch ("http://localhost:3001/inventory");
+}
+
+export async function updateFood() {
+    return fetch ("http://localhost:3001/inventory");
+}
+export async function deleteFood() {
+    return fetch ("http://localhost:3001/inventory");
+}
